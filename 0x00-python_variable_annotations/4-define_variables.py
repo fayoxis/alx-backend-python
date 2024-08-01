507659c584fe5a53979fd3b79ee7a417b42613ae
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 22:08:35 +0100

    Update 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
-"""Task 4's module."""
 
+"""module will annotates the variables"""
 
-def define_variables() -> None:
-    """Define and annotate variables with different types."""
+from typing import Union
+
+def annotate_variables() -> None:
     a: int = 1
     pi: float = 3.14
     i_understand_annotations: bool = True
     school: str = 'Holberton'
 
+    def print_variable(name: str, value: Union[int, float, bool, str]) -> None:
+        print(f"{name} is a {type(value)} with a value of {value}")
 
-if __name__ == "__main__":
-    define_variables()
+    print_variable("a", a)
+    print_variable("pi", pi)
+    print_variable("i_understand_annotations", i_understand_annotations)
+    print_variable("school", school)
+
+if __name__ == '__main__':
+    annotate_variables()
