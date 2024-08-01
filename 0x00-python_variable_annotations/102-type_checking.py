c9309cc3af8b15bb0764e2f6b2f68f09b9153b25
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 22:52:22 +0100

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,25 +6,20 @@ multiplied by certain factor.
 from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
+def zoom_array(lst: tuple, factor: int = 2) -> list:
     """
-    Returns a list of integers multiplied by certain factor.
-
-    Args:
-        lst (Tuple): A tuple of integers.
-        factor (int): An integer representing the multiplication factor.
-
-    Returns:
-        List: A list of integers multiplied by the given factor.
+    Returns a list of integers multiplied by a certain factor.
     """
-    zoomed_list = [item for item in lst for _ in range(factor)]
-    return zoomed_list
+    zoomed_in = []
+    for item in lst:
+        zoomed_in.extend([item] * factor)
+    return zoomed_in
 
 
 array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
-print(zoom_2x)
+print(zoom_2x)  # Output: [12, 12, 72, 72, 91, 91]
 
 zoom_3x = zoom_array(array, 3)
-print(zoom_3x)
+print(zoom_3x)  # Output: [12, 12, 12, 72, 72, 72, 91, 91, 91]
