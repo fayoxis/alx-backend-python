0c879bd2cffedb3f3a1ce5436adc36d0d1389e4d
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 22:29:17 +0100

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -7,7 +7,5 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Creates a function that multiplies a given number by a specified multiplier.
     Args: multiplier (float): The value to multiply the input number with.
-    Returns:  Callable[[float], float]: A function that
-    takes a float and returns a float.
     """
     return lambda x: x * multiplier
