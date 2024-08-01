74178e570316a8d042fdb248ecfb6115547dfc31
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 22:39:06 +0100

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """
 This code demonstrates type annotations for function
-parameters and return values. The `element_length` function
-takes an iterable of sequences and returns a list of tuples,
-where each tuple contains the original sequence and its length.
+parameters and return values. The `element_length`
 """
 from typing import Sequence, Iterable, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    '''Annotating the function parameters'''
+ """ Annotating the function parameters
+ """
     return [(i, len(i)) for i in lst]
 
 if __name__ == '__main__':
