e7bb076ff0aa2845ec796254ef25cd906f1ce77a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 22:21:02 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """Modules."""
-
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Convert a key and its value to a tuple.
-    
     Args:
         k (str): The key.
         v (Union[int, float]): The value, either an integer or float.
