62aa8f9190225135a470efadd4381cac7e87aa13
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 22:21:57 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,11 +6,6 @@ from typing import Union, Tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Convert a key and its value to a tuple.
-    Args:
-        k (str): The key.
-        v (Union[int, float]): The value, either an integer or float.
-    
-    Returns:
-        Tuple[str, float]: A tuple containing the key and the square of its value.
+    Tuple[str, float]: A tuple containing the key and the square of its value.
     """
     return (k, float(v * v))
