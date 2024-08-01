73bb70eba7ad6499fd66b8ba5aeac0947f820b78
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 22:56:08 +0100

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 """
-Contains a function that returns a list of integers
-multiplied by certain factor.
+This module provides a function that takes a tuple of integers
+and a factor as input, and returns a list of integers where each
+element from the original tuple is repeated the specified number
+of times.
 """
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """list of integers multiplied by certain factor.
+    """
+    Multiplies each element in the input tuple by the given factor
+    and returns a new list containing the repeated elements.
+
+    Args:
+        lst (Tuple): The input tuple of integers.
+        factor (int, optional): The number of times each element
+            should be repeated. Defaults to 2.
+
+    Returns:
+        List: A new list containing the repeated elements from
+            the input tuple.
     """
     zoomed_in: List = [
         item for item in lst
