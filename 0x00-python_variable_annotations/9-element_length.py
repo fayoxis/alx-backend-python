46c9dd4e1b4ee92c20b357720bb9215074fb2aa4
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 22:43:28 +0100

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
-""" the below function’s parameters and return
-values
-"""
-from typing import Sequence, Iterable, List, Tuple
+"""this Contains a function with annotated parameters"""
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ Annotating the function """
+def element_length(lst: Iterable[Sequence]
+                   ) -> List[Tuple[Sequence, int]]:
+    """a list of tuples with the length"""
     return [(i, len(i)) for i in lst]
-
-
-if __name__ == '__main__':
-    print(element_length.__annotations__)"
