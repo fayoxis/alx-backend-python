3be80cc3e2528bb96d8078a92413d34a9f4d5cdf
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 22:06:19 +0100

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Module for summing mixed numeric lists."""
-
-
 from typing import List, Union
+
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Sum all elements in a list of integers and floats.
