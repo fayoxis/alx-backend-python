9e11b6922b21779ab0fdc1538f3e1f59b2809b61
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-05 23:57:30 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,13 +12,10 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     delays = []
     tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
-
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
-
     return delays
-
 async def task_wait_random(max_delay: int = 10) -> float:
     """
     Waits for a random delay between 0-max_delay seconds and returns it.
