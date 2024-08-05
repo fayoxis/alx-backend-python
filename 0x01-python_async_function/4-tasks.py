a979d92c28a15f90c2363c5380a326ddae77cbe4
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-06 00:06:42 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -15,7 +15,6 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
-
     return delays
 
 # Function to be imported
