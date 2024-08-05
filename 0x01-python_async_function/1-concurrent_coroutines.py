84887b90cc5b73aa7b0944e6fe79c856cc3f7fb8
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-05 23:35:10 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" modules code ."""
+""" module code."""
 import asyncio
 from typing import List
 
@@ -7,7 +7,8 @@ from utils import wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Executes unknown times."""
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    wait_times = await asyncio.gather(*tasks)
+    """Executes unknow  times."""
+    wait_times = await asyncio.gather(
+        *(wait_random(max_delay) for _ in range(n))
+    )
     return sorted(wait_times)
