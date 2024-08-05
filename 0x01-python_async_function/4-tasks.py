6c666e483fada30886c93eb5abf71dfb6ee9fabb
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-06 00:05:41 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ module code.
 """
+
+
 from typing import List
 import asyncio
 
@@ -10,13 +12,11 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     delays = []
     tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
-
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
 
     return delays
-
 
 # Function to be imported
 async def task_wait_random(max_delay: int) -> float:
