d0fb01f3c09fdf78ef1b2f19583d6cc4a0a449b2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-06 22:25:41 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,8 +9,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Tasks"""
     delays = []
-    tasks = [asyncio.create_task(task_wait_random
-    (max_delay))for _ in range(n)]
+    tasks = [asyncio.create_task(task_wait_random(max_delay))for _ in
+        range(n)]
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
